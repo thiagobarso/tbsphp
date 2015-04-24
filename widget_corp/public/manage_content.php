67f5842b855314088a9c13eea9042afe3fd91c53
thiagosoares <thiagosoares@thiagosoares-HP-Compaq-Elite-8300-SFF>
2015-04-24 17:02:46 -0300

    Video 117

--- widget_corp/public/manage_content.php
+++ widget_corp/public/manage_content.php
@@ -21,6 +21,25 @@
 			<a
 			href="edit_subject.php?subject=<?php echo urlencode($current_subject["id"]); ?>">Edit
 			Subject</a>
+			
+			<div style="margin-top: 2em; border-top: 1px solid #0000000;">
+			<h3>Pages in this subject:</h3>
+			<ul>
+				<?php 
+					$subject_pages = find_pages_for_subject($current_subject["id"]);
+					while($page = mysqli_fetch_assoc($subject_pages)){
+						echo "<li>";
+						$safe_page_id = urlencode($page["id"]);
+						echo "<a href=\"menage_content.php?page={$safe_page_id}\">";
+						echo htmlentities($page["menu_name"]);
+						echo "</a>";
+						echo "</li>";
+					}
+				?>
+			</ul>
+			<br />
+			+ <a href="new_page.php?subject=<?php echo urlencode($current_subject["id"]);?>">Add a new page to this subject</a>
+			</div>
 		<?php } elseif ($current_page){?>
 			<h2>Manage Page</h2>
 			Menu name: <?php echo htmlentities($current_page["menu_name"]);?> <br />
@@ -29,7 +48,10 @@
 			Content:<br />
 			<div class="view-content">
 				<?php echo htmlentities($current_page["content"]); ?>
-			</div>			
+			</div>
+			<br />
+			<br />
+			<a href="edit_page.php?page=<?php echo urlencode($current_page['id']); ?>">Edit Page</a>			
 		<?php } else {?>
 			Please select a subject or a page.
 		<?php } ?>
